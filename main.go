d248e10f8de2e5cce567a9053d957f33f3b7ef79
xChen16 <ares_0427@163.com>
2021-07-04 21:08:15 +0800

    DownloadHandler

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc", handler.UploaSucdHandler)
 	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("failed to start")
